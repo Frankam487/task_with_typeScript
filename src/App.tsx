38300386330bc03977bf0d0ad2806a473715e9ce
Frankam487 <fkamgang06@gmail.com>
2025-04-21 11:00:04 -0700

    handleSubmit function added

--- src/App.tsx
+++ src/App.tsx
@@ -12,21 +12,43 @@ function App() {
       name: "Faire du nodeJS"
     },
   ])
-
+  const handleClick = (id: number) => {
+    const taskCopy = [...data];
+    const taskUpdate = taskCopy.filter((t) => t.id != id);
+    setData(taskUpdate);
+  }
+  const handleSubmit = () => {
+    
+    
+  };
   return (
     <div className="">
       <h1>Liste des taches a faire cette semaine !!!!!!</h1>
       <div className="">
         <ul className=" flex flex-col justify-center align-center">
-
-        {data.map((task, index) => (
-          <li key={index}>{task.name} <button className="p-2 bg-gray-300 hover:bg-red">X</button></li>
-        ))}
+          {data.map((task, index) => (
+            <li key={index}>
+              {task.name}{" "}
+              <button
+                className="p-2 bg-gray-900 hover:bg-red"
+                onClick={() => handleClick(task.id)}
+              >
+                X
+              </button>
+            </li>
+          ))}
         </ul>
-        <form action="">
-          <label htmlFor="">Entre une nouvelle tache a faire cette semaine...</label>
-          <input type="text"  className="border" placeholder="Entrer une tache" onChange={(e) => setChange(e.target.value)} />
-          <input type="submit" value="Valider" id="" />
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="">
+            Entre une nouvelle tache a faire cette semaine...
+          </label>
+          <input
+            type="text"
+            className="border"
+            placeholder="Entrer une tache"
+            onChange={(e) => console.log(e.target.value)}
+          />
+          <input type="submit" value="Valider" />
         </form>
       </div>
     </div>
